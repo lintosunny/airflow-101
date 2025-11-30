87ae346661a9e58a063730d44f70dee4ef380208
lintosunny <lintosunny111@gmail.com>
2025-11-30 13:21:33 +0530

    first dag

---
+++ dags/celery.py
@@ -0,0 +1,29 @@
+from airflow.sdk import dag, task
+from time import sleep
+
+@dag
+def celery_dag():
+
+    @task
+    def a():
+        sleep(5)
+
+    @task(
+        queue="high_cpu"
+    )
+    def b():
+        sleep(5)
+
+    @task
+    def c():
+        sleep(5)
+
+    @task(
+            queue="high_cpu"
+    )
+    def d():
+        sleep(5)
+
+    a() >> [b(), c()] >> d()
+
+celery_dag()
